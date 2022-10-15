aaef13d669575c78773a90a254f5fa966175b18a
AbdulQadir92 <a_qadir_jamali@hotmail.com>
2022-10-15 23:27:26 +0500

    Resolved a minor bug in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -25,10 +25,21 @@ const Navbar = () => {
     const makeActive = () => {
         const links = document.querySelectorAll('.nav-link');
         links.forEach(link => {
-            link.addEventListener('click', (e) => {
+          
+            if (link.innerText.toLowerCase() === loc.pathname.slice(1)) {
+                link.classList.add('active');
+            } else {
+                link.classList.remove('active');
+            }
+
+            if(link.innerText.toLowerCase() === 'home' && loc.pathname === '/') {
+                link.classList.add('active');
+            }
+
+            link.addEventListener('click', () => {
                 links.forEach(link => {
                     if (link.classList.contains('active')) {
-                        link.classList.remove('active')
+                        link.classList.remove('active');
                     }
                 })
                 link.classList.add('active');
